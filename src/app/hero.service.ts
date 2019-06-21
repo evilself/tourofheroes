3454243c22ca114f31219a04d835c673c8981544
Boris Mechkov <borismechkov@gmail.com>
2019-06-21 22:38:14 +0300

    Added adding and updating

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -12,6 +12,7 @@ export class HeroService {
 
   private heroesUrl = 'api/heroes';  // URL to web api
 
+  
   constructor( private http: HttpClient,
                private messageService: MessageService) { }
 
@@ -56,5 +57,27 @@ export class HeroService {
       // Let the app keep running by returning an empty result.
       return of(result as T);
     };
+  }
+
+  /** PUT: update the hero on the server */
+  updateHero (hero: Hero): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    };
+    return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
+
+  /** POST: add a new hero to the server */
+  addHero (hero: Hero): Observable<Hero> {
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    };
+    return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
   }
 }
